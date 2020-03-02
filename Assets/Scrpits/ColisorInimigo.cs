4e75407c1056805cdb580d7f68a4160cfd689585
Rafael Gomes Pinheiro <RafaelGomes00@users.noreply.github.com>
2020-03-02 17:41:12 -0300

    Script perseguição
    Script de perseguição do inimigo por area

---
+++ Assets/Scrpits/ColisorInimigo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ColisorInimigo : MonoBehaviour
+{
+    public Transform target;
+    public NavMeshAgent NavMesh;
+    float distance = 0;
+    public Transform posicaoInicial;
+
+    private void OnTriggerStay(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            NavMesh.SetDestination(target.position);
+        }
+    }
+    private void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            NavMesh.SetDestination(posicaoInicial.position);
+        }
+    }
+}
