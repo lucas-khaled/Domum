273e7811e84ef98a8d6435f81a4fcd9474512c14
Rafael Gomes Pinheiro <RafaelGomes00@users.noreply.github.com>
2020-03-23 19:12:38 -0300

    Script ataque inimigos distancia

---
+++ Assets/Scrpits/Inimigos/Distancia.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Distancia : Inimigo
+{
+    public GameObject imagemAlerta;
+    public GameObject bala;
+    public Transform pontoTiro;
+    protected override IEnumerator Atacar()
+    {
+        //tocar animação de mirar
+        Transform transform = this.gameObject.transform;
+        transform.position = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y, this.gameObject.transform.position.z);
+        GameObject gameObject = Instantiate(imagemAlerta, transform);
+
+        yield return new WaitForSeconds(0.2f);
+
+        Destroy(gameObject);
+
+        Instantiate(bala, pontoTiro);
+        //tocar animação de tiro
+
+        yield return new WaitForSeconds(0.5f);
+
+        ataqueCooldown = velocidadeAtaque;
+
+    }
+}
