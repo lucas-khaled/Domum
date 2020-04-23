dc82b06b0292233cfde2f85d950e4a2b4fecb27f
Rafael Gomes Pinheiro <RafaelGomes00@users.noreply.github.com>
2020-04-23 12:16:38 -0300

    FSM Animais (Incompleto)

--- Assets/Testes/Animais/Deitar.cs
+++ Assets/Testes/Animais/Deitar.cs
@@ -2,19 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Caminhar : StateMachineBehaviour
+public class Deitar : StateMachineBehaviour
 {
+    private float aux = 30;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetBool("Deitado",true);
+    }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (aux <= 0)
+        {
+            int random = Random.Range(1, 4);
+            if (random == 1)
+            {
+                animator.SetTrigger("Levantar");
+                animator.SetBool("Deitado", false);
+            }
+            aux = 30;
+        }
+        else
+        {
+            aux = aux - Time.deltaTime;
+        }
+    }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
---
+++ Assets/Testes/Animais/Idle.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Idle : StateMachineBehaviour
+{
+    private double aux = 0;
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (aux <= 0)
+        {
+            int random = Random.Range(1, 4);
+            if (random == 1)
+            {
+                animator.ResetTrigger("Deitar");
+                animator.SetBool("Caminhando", true);
+                animator.SetBool("Deitado", false);
+                //comer ou andar
+            }
+            if (random == 2)
+            {
+                random = Random.Range(1, 3);
+                if (random == 1)
+                {
+                    animator.SetBool("Caminhando",false);
+                    animator.SetTrigger("Deitar");
+                    //ficar parado ou deitar
+                }
+            }
+            aux = 10;
+        }
+        else
+        {
+            aux = aux - Time.deltaTime;
+        }
+    }
+}
