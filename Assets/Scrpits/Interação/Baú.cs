e6a91dde99cfaa9b1c0fce5e873664ccbbe8ce1d
Rafael Gomes Pinheiro <RafaelGomes00@users.noreply.github.com>
2020-05-04 22:14:35 -0300

    Baú

---
+++ Assets/Scrpits/Interação/Baú.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Baú : Interagivel
+{
+    [SerializeField]
+    List<Item> itens = new List<Item>();
+    [SerializeField]
+    private Item[] vetItens;
+    [SerializeField]
+    Animator anim;
+    /*private void GeraItens()
+    {
+        int escolha = Random.Range(1, 5);
+        for (int i = 0; i < escolha; i++)
+        {
+            int itemEsc = Random.Range(0, vetItens.Length);
+            itens.Add(vetItens[itemEsc]);
+        }
+    }*/
+    public override void Interact()
+    {
+        if (Inventario.inventario.AddItem(itens[0]))
+        {
+            Debug.Log("IEI");
+        }
+        anim.SetBool("Aberto", true);
+    }
+}
