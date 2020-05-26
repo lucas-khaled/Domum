bb3db90bfe99c49b4d7068bd0c1dd2191a0cef72
samuel-lagostin <61011499+samuel-lagostin@users.noreply.github.com>
2020-05-26 14:30:39 -0300

    Player_Map

--- Assets/Scrpits/Camera/CameraFollow.cs
+++ Assets/Scrpits/Camera/CameraFollow.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-
     private void LateUpdate()
     {
+
+        transform.parent = Player.player.transform;
         Vector3 newPosition = Player.player.transform.position;
         newPosition.y = transform.position.y;
         transform.position = newPosition;
