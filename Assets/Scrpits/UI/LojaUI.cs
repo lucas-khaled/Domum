40311c04a527860da1e77b316d717775b1c7efe7
Rafael Gomes Pinheiro <61011140+RafaelGomes00@users.noreply.github.com>
2020-09-29 15:04:38 -0300

    Feedback fama insuficiente loja

--- Assets/Scrpits/UI/LojaUI.cs
+++ Assets/Scrpits/UI/LojaUI.cs
@@ -20,6 +20,8 @@ public class LojaUI : MonoBehaviour
     private Text inventarioPeso;
     [SerializeField]
     private Animator Dinherio_insu;
+    [SerializeField]
+    private Animator Fama_insu;
 
     [Header("InfoItem")]
     [SerializeField]
@@ -280,9 +282,14 @@ public class LojaUI : MonoBehaviour
 
     public void ComprarItem()
     {
-        if(Player.player.status.Dinheiro < holderAtual.item.custoMoeda)
+        if (Player.player.status.Dinheiro < holderAtual.item.custoMoeda)
         {
             Dinherio_insu.SetTrigger("Dinheiro_insu");
+            return;
+        }
+        else if (Player.player.status.Fama < int.Parse(itemFama.text))
+        {
+            Fama_insu.SetTrigger("Fama_insu");
             return;
         }
         else
