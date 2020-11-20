0495907e59476a3d3046e11e06d8002b6d08da75
Rafael Gomes Pinheiro <61011140+RafaelGomes00@users.noreply.github.com>
2020-11-20 12:51:38 -0300

    arrumei rapidinho

--- Assets/Scrpits/Inimigos/RespawnInimigos.cs
+++ Assets/Scrpits/Inimigos/RespawnInimigos.cs
@@ -33,24 +33,20 @@ public class RespawnInimigos : MonoBehaviour
 
             numeroInimigos = this.transform.childCount;
 
-            if (Vector3.Distance(Player.player.gameObject.transform.position, this.transform.position) < raioSpawn + 5f)
-                yield return new WaitForSeconds(30f);
-            else
+            while (numeroInimigos < numeroMaximo)
             {
-                while (numeroInimigos < numeroMaximo)
+                Vector3 novoSpawn = RandomNavMeshGenerator();
+                Collider[] hit = Physics.OverlapSphere(novoSpawn, 2f, LayerMask.GetMask("Ground"));
+                if (hit.Length > 0)
                 {
-                    Vector3 novoSpawn = RandomNavMeshGenerator();
-                    Collider[] hit = Physics.OverlapSphere(novoSpawn, 2f, LayerMask.GetMask("Ground"));
-                    if (hit.Length > 0)
-                    {
-                        Instantiate(inimigos[Random.Range(0, inimigos.Length)], novoSpawn, Quaternion.identity, parentescoHierarquia);
-                        numeroInimigos++;
-                    }
+                    Instantiate(inimigos[Random.Range(0, inimigos.Length)], novoSpawn, Quaternion.identity, parentescoHierarquia);
+                    numeroInimigos++;
                 }
-                yield return new WaitForSeconds(118f);
             }
-            
-        }
+            yield return new WaitForSeconds(118f);
+
+        }     
+        
     }
 
     public Vector3 RandomNavMeshGenerator()
